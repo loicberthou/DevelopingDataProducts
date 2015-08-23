6c55a83e7b99e3b46e9e998188ba1728242f0545
Loweek <loicberthou@yahoo.fr>
2015-08-23 16:36:52 -0400

    Shiny App mostly finalized

--- server.R
+++ server.R
@@ -1,8 +1,38 @@
 library(shiny)
+library(ggplot2)
+library(datasets)
+
+set.seed(20150822)
+
+data("mtcars")
 
 shinyServer(
   function(input, output) {
-    output$text1 <- renderText({input$text1})
-    output$text2 <- renderText({input$text2})
+
+    output$linearRegFormula <- renderText({
+      if(input$corrType=="simple") {
+        "lm(mpg ~ wt + factor(am), data=mtcars)"
+      }
+      else if(input$corrType=="complex") {
+        "lm(mpg ~ wt * factor(am), data=mtcars)"
+      }
+      else if(input$corrType=="final") {
+        "lm(mpg ~ wt * factor(am) + qsec, data=mtcars)"
+      }
+    })
+    output$plotResid <- renderPlot({
+      if(input$corrType=="simple") {
+        fit <- lm(mpg ~ wt + factor(am), data=mtcars)
+      }
+      else if(input$corrType=="complex") {
+        fit <- lm(mpg ~ wt * factor(am), data=mtcars)
+      }
+      else if(input$corrType=="final") {
+        fit <- lm(mpg ~ wt * factor(am) + qsec, data=mtcars)
+      }
+      par(mfrow=c(2,2))
+      plot(fit)
+      par(mfrow=c(1,1))
+    })
   }
 )
--- ui.R
+++ ui.R
@@ -1,13 +1,24 @@
 shinyUI(
   pageWithSidebar(
-    headerPanel("MPG / Parameters"),
+    headerPanel("Motor Trend Car Road Tests - Mileage analysis"),
     sidebarPanel(
-      textInput(inputId="text1", label = "Input text 1"),
-      textInput(inputId="text2", label = "Input text 2")
-    ),
+      radioButtons("corrType", "Correlation parameters:",
+                   c("wt + factor(am)" = "simple",
+                     "wt * factor(am)" = "complex",
+                     "wt * factor(am) + qsec" = "final"),
+      selected = "simple")
+      ),
     mainPanel(
-      p("Output Text 1"), textOutput("text1"),
-      p("Output Text 2"), textOutput("text2")
+      h3("How to use this Shiny App:"),
+      p("In the mtcars dataset provided with R Studio, we are trying to assess regression model
+        that will fit the best and help us understand which parameters have the most impact on
+        the mileage."),
+      p("In this Shiny application, you can select the three models that was studying and see
+        the residuals for each one. By clicking on each one on the left side, you can see the
+        impact on the residuals and hopefully agree with my conclusions."),
+      h4("The linear regression formula used is: "),
+      verbatimTextOutput("linearRegFormula"),
+      plotOutput('plotResid')
     )
   )
 )
